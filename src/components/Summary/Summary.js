6a161b37a02660e363d6493061599dbfb193ad1b
Anuchit Prasertsang <anuchit.prasertsang@gmail.com>
2020-02-21 22:32:03 +0700

    example summary

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,5 +1,28 @@
 import React from "react";
+import styles from "./Summary.module.scss";
 
-const Summary = () => <div>Summary</div>;
+const Summary = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.underHerder} />
+      <div className={styles.items}>
+        {Array(100)
+          .fill()
+          .map(x => (
+            <div className={styles.item}>
+              <div className={styles.left}>
+                <div className={styles.logo}>logo</div>
+                <div>
+                  <div className={styles.title}>Netflix</div>
+                  <div className={styles.helper}>15 Mar 2020, 8:20PM</div>
+                </div>
+              </div>
+              <div className={styles.right}>- $10</div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default Summary;
