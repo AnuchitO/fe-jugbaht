3248085de859624f171cfe5378beb034a212638c
Anuchit Prasertsang <anuchit.prasertsang@gmail.com>
2020-02-21 23:15:15 +0700

    ordering element

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,6 +1,14 @@
 import React from "react";
 import styles from "./Summary.module.scss";
 
+import {
+  ListItemAvatar,
+  Avatar,
+  ListItemIcon,
+  ListItemText
+} from "@material-ui/core";
+import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
+
 const Summary = () => {
   return (
     <div className={styles.root}>
@@ -11,13 +19,33 @@ const Summary = () => {
           .map(x => (
             <div className={styles.item}>
               <div className={styles.left}>
-                <div className={styles.logo}>logo</div>
-                <div>
-                  <div className={styles.title}>Netflix</div>
-                  <div className={styles.helper}>15 Mar 2020, 8:20PM</div>
-                </div>
+                <ListItemAvatar>
+                  <Avatar />
+                  <ListItemText primary={<div>เขียบ</div>} />
+                </ListItemAvatar>
               </div>
-              <div className={styles.right}>- $10</div>
+              <div className={styles.middle}>
+                <ListItemIcon>
+                  <DoubleArrowIcon />
+                  <DoubleArrowIcon />
+                </ListItemIcon>
+              </div>
+              <div className={styles.middle}>
+                <div className={styles.title}>฿ 999.99</div>
+                {/* <div className={styles.helper}>15 Mar 2020, 8:20PM</div> */}
+              </div>
+              <div className={styles.middle}>
+                <ListItemIcon>
+                  <DoubleArrowIcon />
+                  <DoubleArrowIcon />
+                </ListItemIcon>
+              </div>
+              <div className={styles.right}>
+                <ListItemAvatar>
+                  <Avatar />
+                  <ListItemText primary={<div>หน่อง</div>} />
+                </ListItemAvatar>
+              </div>
             </div>
           ))}
       </div>
